55970e12af6162b997d90d1eb20212d8922f3fa4
Jay Singh <jaysinghrajput265@gmail.com>
2026-05-21 15:52:44 +0530

    Fixed all API routes

--- src/app.js
+++ src/app.js
@@ -1,31 +1,34 @@
-const express = require('express');
-const cookieParser = require('cookie-parser')
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
 
-const cors = require('cors');
+const app = express();
 
+app.use(express.json());
 
-const app = express()
-app.use(express.json())
-app.use(cookieParser())
-app.use(cors({
-  credentials: true,
-  origin: "https://social-media-frontend-gules-five.vercel.app",
-}));
+app.use(cookieParser());
 
-const authRouter = require('./routes/auth.route')
-const postRouter = require('./routes/post.routes')
-const userRouter = require('./routes/user.route')
-const commentRouter = require('./routes/comment.routes')
+app.use(
+  cors({
+    origin: "https://social-media-frontend-gules-five.vercel.app",
+    credentials: true,
+  })
+);
+
+const authRouter = require("./routes/auth.route");
+const postRouter = require("./routes/post.routes");
+const userRouter = require("./routes/user.route");
+const commentRouter = require("./routes/comment.routes");
 const messageRouter = require("./routes/message.routes");
 
-
-
+app.use("/api/auth", authRouter);
+app.use("/api/posts", postRouter);
+app.use("/api/users", userRouter);
+app.use("/api/comments", commentRouter);
 app.use("/api/messages", messageRouter);
 
-app.use('/api/auth',authRouter)
-app.use('/api/posts',postRouter)
-app.use('/api/users',userRouter)
-app.use("/api/comments", commentRouter);
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
 
-
-module.exports = app 
+module.exports = app;
