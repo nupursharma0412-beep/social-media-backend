1e65ed0e9e255753c31e4c6e6c01306d035b3544
NUPUR SHARMA <nupursharma0412@gmail.com>
2026-06-10 17:27:47 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const app = require("./src/app");
 const connectDB = require("./src/config/db");
 
 const http = require("http");
-const { Server } = require("socket.io");
+const { Server } = require("socket.io"); 
 
 connectDB();
 
